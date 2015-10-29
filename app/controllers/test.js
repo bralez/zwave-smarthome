b0c5d1b18bef417f33fd90a3756412808278397c
Martin Vach <mv@zwaveeurope.com>
2015-10-29 16:21:10 +0100

    Test controller integration

---
+++ app/controllers/test.js
@@ -0,0 +1,17 @@
+/**
+ * Application test controller
+ * @author Martin Vach
+ */
+
+myAppController.controller('TestController', function($scope, dataFactory,dataService) {
+    $scope.input = {};
+    
+    /**
+     * Load ZwaveApiData
+     */
+    $scope.loadData = function() {
+        
+    };
+
+    $scope.loadData();
+});
